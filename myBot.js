82183b0e22ced9c566b7897657a81fedf32990a2
Zahra Shahid <zashahid45@gmail.com>
2023-01-16 02:07:18 +0500

    guilds displayed

--- myBot.js
+++ myBot.js
@@ -1,11 +1,22 @@
 const Discord = require("discord.js");
-const {GatewayIntentBits} = require("discord.js")
-const {TOKEN} = require("./config")
+const { GatewayIntentBits } = require("discord.js");
+const { TOKEN } = require("./config");
 
-const client = new Discord.Client({intents: [GatewayIntentBits.Guilds,GatewayIntentBits.GuildMessages]})
+const client = new Discord.Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
+});
 
-client.on("ready",()=>{
-    console.log("Connected as "+ client.user.tag)
-})
+client.on("ready", () => {
+  console.log("Connected as " + client.user.tag);
 
-client.login(TOKEN)
+  // the status to be displayed under the user
+  // client.user.setActivity(" with JavaScript")
+  client.user.setActivity("Youtube", { type: "WATCHING" });
+
+  // displaying each guild
+    [client.guilds].forEach(guild => {
+      console.log(guild);
+    });
+});
+
+client.login(TOKEN);
